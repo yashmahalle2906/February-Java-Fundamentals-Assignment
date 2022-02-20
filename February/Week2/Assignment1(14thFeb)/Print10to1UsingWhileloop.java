ad4bf4fea644e1be33de02ae0c2ed6c42ae923b5
yashmahalle2906 <97544847+yashmahalle2906@users.noreply.github.com>
2022-02-20 14:34:00 +0530

    Add files via upload

---
+++ February/Week2/Assignment1(14thFeb)/Print10to1UsingWhileloop.java
@@ -0,0 +1,15 @@
+package JavaFundamentals;
+
+public class Print10to1UsingWhileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		while(i>=1) {
+			System.out.println(i);
+			i--;
+
+	}
+
+}
+}
