2504825cde5dc8ccbae3b2a698e406d51d2ca76c
yashmahalle2906 <97544847+yashmahalle2906@users.noreply.github.com>
2022-02-20 14:35:14 +0530

    Add files via upload

---
+++ February/Week2/Assignment1(14thFeb)/PrintNumbersDivisibleBy3From1To100.java
@@ -0,0 +1,16 @@
+package JavaFundamentals;
+
+public class PrintNumbersDivisibleBy3From1To100 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=100;i++) {
+			if(i%3==0) {
+				System.out.println(i);
+			}
+		}
+
+		
+	}
+
+}
