3659d8354c27031971a32f4152ba785caa235d0c
yashmahalle2906 <97544847+yashmahalle2906@users.noreply.github.com>
2022-02-22 16:39:03 +0530

    Add files via upload

---
+++ February/Week 3/Assignment1(21feb)/AND_OR_XOR.java
@@ -0,0 +1,30 @@
+package JavaFundamentals;
+
+public class AND_OR_XOR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 7;
+		int b = 5;
+
+		System.out.println("a & b is " + (a & b));
+		System.out.println("a | b is " + (a | b));
+		System.out.println("a ^ b is " + (a ^ b));
+
+		int c = 17;
+		int d = 9;
+
+		System.out.println("c & d is " + (c & d));
+		System.out.println("c | d is " + (c | d));
+		System.out.println("c ^ d is " + (c ^ d));
+
+		int e = 91;
+		int f = 56;
+
+		System.out.println("e & f is " + (e & f));
+		System.out.println("e | f is " + (e | f));
+		System.out.println("e ^ f is " + (e ^ f));
+
+	}
+
+}
